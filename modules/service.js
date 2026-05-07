e45f78d53c1e9afcba6c9ac1032dcca6fa8e5c3f
Pathargata Digital Community Ltd <mdkamrulmunshi998@gmail.com>
2026-05-07 22:04:14 +0600

    Add loadService function to service.js

---
+++ modules/service.js
@@ -0,0 +1,11 @@
+export function loadService() {
+    return `
+        <h3>উপজেলা সেবাসমূহ</h3>
+        <div class="service-grid">
+            <div class="service-item"><i class="fas fa-tint" style="color:red"></i><br>রক্তদান</div>
+            <div class="service-item"><i class="fas fa-phone-alt" style="color:green"></i><br>জরুরি কল</div>
+            <div class="service-item"><i class="fas fa-book" style="color:blue"></i><br>ডিরেক্টরি</div>
+            <div class="service-item"><i class="fas fa-ambulance" style="color:orange"></i><br>অ্যাম্বুলেন্স</div>
+        </div>
+    `;
+}
